d2f182fda6c9562c40c3f4827a93ec908b76a9b3
Julian Pollak <j.pollak@fruitcake.ch>
2024-07-17 11:39:31 +0200

    update info, adjust dependencies

--- WireMailSendGrid.info.php
+++ WireMailSendGrid.info.php
@@ -12,14 +12,15 @@ $info = [
     'version'    => 113,
     'summary'    => "Extend WireMail to bypass PHP Mail and send mail via SendGrids Web API",
 
-    'author'     => "The Big Surf",
-    'href'       => "https://github.com/theGC/WireMailSendGrid",
+    'author'     => "poljpocket, original author: The Big Surf",
+    'href'       => "https://github.com/poljpocket/WireMailSendGrid",
 
     'autoload'   => false,
     'singular'   => false,
 
     'requires'   => [
-        "ProcessWire>=2.8",
+        "ProcessWire>=3.0",
+        "PHP>=8.0",
     ],
 
 ];
